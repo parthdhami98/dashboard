3359dc64ab66f5ca452869531bfc2d2c87bfba2c
Parth Dhami <51398563+parthdhami98@users.noreply.github.com>
2020-01-20 16:27:00 +0530

    assignment

--- src/components/DetailData.js
+++ src/components/DetailData.js
@@ -1,30 +1,76 @@
 import React from "react";
-import { Card, Badge, Divider, Descriptions } from "antd";
+import {
+  Card,
+  Upload,
+  message,
+  Button,
+  Icon,
+  Divider,
+  Descriptions
+} from "antd";
 
 export default function DetailData(props) {
   const { singleuser } = props;
+
+  const fileprops = {
+    name: "file",
+    action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+    headers: {
+      authorization: "authorization-text"
+    },
+    onChange(info) {
+      if (info.file.status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === "done") {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    }
+  };
+
   return (
     <div className="detail">
-      <h2 style={{ textAlign: "center" }}> Employee Details</h2>
+      <h2 style={{ textAlign: "center" }}> Article Details</h2>
       <Divider />
       <Card>
         <Descriptions bordered>
-          <Descriptions.Item label="ID" span={3}>
-            {singleuser.id}
+          <Descriptions.Item label="Article Code" span={3}>
+            {"Art -" + singleuser.id}
           </Descriptions.Item>
-          <Descriptions.Item label="Name" span={3}>
-            {singleuser.employee_name}
+          <Descriptions.Item label="Topic" span={3}>
+            {singleuser.topic}
           </Descriptions.Item>
-          <Descriptions.Item label="Age" span={3}>
-            {singleuser.employee_age}
+          <Descriptions.Item label="Words" span={3}>
+            {singleuser.words}
           </Descriptions.Item>
-          <Descriptions.Item label="Salary" span={3}>
-            {singleuser.employee_salary}
+          <Descriptions.Item label="Deadline" span={3}>
+            {singleuser.deadline}
           </Descriptions.Item>
-          <Descriptions.Item label="Status" span={3}>
-            <Badge status="success" text="Verified" />
+          <Descriptions.Item label="Genre" span={3}>
+            {singleuser.Genre}
+          </Descriptions.Item>
+          <Descriptions.Item label="Vertical" span={3}>
+            {singleuser.vertical}
+          </Descriptions.Item>
+          <Descriptions.Item label="Description" span={3}>
+            {singleuser.paragraph}
+          </Descriptions.Item>
+          <Descriptions.Item label="File Upload" span={3}>
+            <Upload {...fileprops}>
+              <Button>
+                <Icon type="upload" /> Click to Upload
+              </Button>
+            </Upload>
           </Descriptions.Item>
         </Descriptions>
+        <div
+          style={{ display: "flex", justifyContent: "center", marginTop: 10 }}
+        >
+          <Button>Accept</Button>
+          <Button style={{ marginLeft: 5 }}>Reject</Button>
+        </div>
       </Card>
     </div>
   );
--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import DetailData from "./DetailData";
-import axios from "axios";
+import Moment from "moment";
+import faker from "faker";
 import "./Assignment.css";
 import { Card, Spin, Menu, Layout, Icon, Row, Col } from "antd";
 import { Table } from "react-bootstrap";
@@ -17,121 +18,134 @@ const List = props => {
   const [postperpage] = useState(10);
 
   useEffect(() => {
-    getdata();
-  }, []);
-
-  const getdata = async () => {
-    let error;
-    const url = "http://dummy.restapiexample.com/api/v1/employees";
-    try {
-      let response = await axios.get(url);
-      if (response.status !== 200) {
-        throw new error("something went wrong");
-      }
-      const data = response.data.data;
-
-      setuserdata(data);
-      setLoading(false);
-    } catch (e) {
-      console.log(e);
+    let getusers = [];
+    for (let i = 0; i < 50; i++) {
+      getusers.push({
+        Genre: "business",
+        id: faker.random.alphaNumeric(),
+        topic: faker.lorem.sentence(),
+        deadline: "20/1/2020",
+        vertical: "Blog Post",
+        words: faker.lorem.paragraph().length,
+        paragraph: faker.lorem.paragraph()
+      });
     }
-  };
+    setuserdata(getusers);
+  }, []);
 
   const showdata = user => {
     setshowdetails(true);
     setsingleuser(user);
   };
-
+  console.log(userdata);
   const indexOfflastpost = currentpage * postperpage;
   const indexOffirstpost = indexOfflastpost - postperpage;
   const currentpost = userdata.slice(indexOffirstpost, indexOfflastpost);
   const paginate = pageNumber => setcurrentpage(pageNumber);
 
   return (
-    <Layout>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={broken => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1">
-            <Icon type="user" />
-            <span className="nav-text">Profile</span>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Icon type="video-camera" />
-            <span className="nav-text">Project</span>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Icon type="upload" />
-            <span className="nav-text">Payment</span>
-          </Menu.Item>
-          <Menu.Item key="4">
-            <Icon type="user" />
-            <span className="nav-text">Feedback</span>
-          </Menu.Item>
-          <Menu.Item key="5">
-            <Icon type="user" />
-            <span className="nav-text">Message Center</span>
-          </Menu.Item>
-        </Menu>
-      </Sider>
+    <div>
       <Layout>
-        <Header style={{ background: "rgb(0,21,41)", height: 69 }} />
-        <Content style={{ margin: "24px 16px 0" }}>
-          <Row>
-            <Col span={12}>
-              <div className="list">
-                <Card>
-                  <Spin spinning={Loading}>
-                    <Table striped bordered hover size="sm">
-                      <thead>
-                        <tr>
-                          <th>ID</th>
-                          <th>Name</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {currentpost.map((user, index) =>
-                          <tr key={index}>
-                            <td>
-                              {user.id}
-                            </td>
-
-                            <td>
-                              <a onClick={() => showdata(user)} href="#">
-                                {user.employee_name}
-                              </a>
-                            </td>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={broken => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span className="nav-text">Profile</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span className="nav-text">Project</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span className="nav-text">Payment</span>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <Icon type="user" />
+              <span className="nav-text">Feedback</span>
+            </Menu.Item>
+            <Menu.Item key="5">
+              <Icon type="user" />
+              <span className="nav-text">Message Center</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: "rgb(0,21,41)", height: 69 }} />
+          <Content>
+            <Row>
+              <Col span={15}>
+                <div className="list">
+                  <Card>
+                    <Spin spinning={Loading}>
+                      <Table striped bordered hover size="sm">
+                        <thead>
+                          <tr>
+                            <th>Article Code</th>
+                            <th>Topic</th>
+                            <th>Words</th>
+                            <th>Genre</th>
+                            <th>Deadline</th>
+                            <th>Vertical</th>
                           </tr>
-                        )}
-                      </tbody>
-                    </Table>
-                    <Pagination
-                      postperpage={postperpage}
-                      totalpage={userdata.length}
-                      paginate={paginate}
-                    />
-                  </Spin>
-                </Card>
-              </div>
-            </Col>
-            {showdetails
-              ? <Col span={12}>
-                  <DetailData singleuser={singleuser} />
-                </Col>
-              : null}
-          </Row>
-        </Content>
+                        </thead>
+                        <tbody>
+                          {currentpost.map((user, index) =>
+                            <tr key={index}>
+                              <td>
+                                {"Art-" + user.id}
+                              </td>
+
+                              <td>
+                                <a onClick={() => showdata(user)} href="#">
+                                  {user.topic}
+                                </a>
+                              </td>
+                              <td>
+                                {user.words}
+                              </td>
+                              <td>
+                                {user.Genre}
+                              </td>
+                              <td>
+                                {`${user.deadline}`}
+                              </td>
+                              <td>
+                                {user.vertical}
+                              </td>
+                            </tr>
+                          )}
+                        </tbody>
+                      </Table>
+                      <Pagination
+                        postperpage={postperpage}
+                        totalpage={userdata.length}
+                        paginate={paginate}
+                      />
+                    </Spin>
+                  </Card>
+                </div>
+              </Col>
+              {showdetails
+                ? <Col span={9}>
+                    <DetailData singleuser={singleuser} />
+                  </Col>
+                : null}
+            </Row>
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </div>
   );
 };
 export default List;
